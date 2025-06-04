a7e0f85056349f2b54681498b118379f841e0896
LDEMON1020 <LD_EMON1020@g.shingu.ac.kr>
2025-06-04 15:55:42 +0900

    [Feat] 게임 스코어 Text, 탑뷰 움직임 구현

---
+++ Assets/Player.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Player : MonoBehaviour
+{
+    float moveSpeed = 2f;
+
+    public TextMeshProUGUI ScoreText;
+
+    public float score;
+
+    [SerializeField] Sprite spriteUp;
+    [SerializeField] Sprite spriteDown;
+    [SerializeField] Sprite spriteLeft;
+    [SerializeField] Sprite spriteRight;
+
+    Rigidbody2D rb;
+    SpriteRenderer sR;
+
+    Vector2 input;
+    Vector2 velocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sR = GetComponent<SpriteRenderer>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        score = 0f;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        velocity = input.normalized * moveSpeed;
+
+        if(input.sqrMagnitude > .01f)
+        {
+            if(Mathf.Abs(input.x) > Mathf.Abs(input.y))
+            {
+                if (input.x > 0)
+                    sR.sprite = spriteRight;
+                else if (input.x < 0)
+                    sR.sprite = spriteLeft;
+            }
+            else
+            {
+                if (input.y > 0)
+                    sR.sprite = spriteUp;
+                else
+                    sR.sprite = spriteDown;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Item"))
+        {
+            score += collision.GetComponent<ItemObject>().GetPoint();
+            Destroy(collision.gameObject);
+            ScoreText.text = "Score : " + score;
+        }
+
+       
+    }
+}
